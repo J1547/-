359c7adc3442ed1fa1f34c0f67e9415032a65411
J1547 <J1547@users.noreply.github.com>
2019-07-16 11:52:04 +0800

    none

---
+++ wp-theme/fosu mobile/header.php
@@ -0,0 +1,46 @@
+﻿<!DOCTYPE html>
+<meta http-equiv="X-UA-Compatible" content="IE=8">
+<html lang="zh-CN">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="renderer" content="webkit|ie-comp|ie-stand">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="keywords" content="佛山科学技术学院,佛山大学,lambda工作室,凌达工作室">
+    <meta name="description" content="佛山科学技术学院，原佛山大学，国家硕士学位授予单位，全日制本科大学，坐落于国家历史文化名城——广东省佛山市。已发展成为一所拥有理学、经济学、法学、教育学、文学、工学、农学、医学、管理学、艺术学等学科门类，...">
+    <title>佛山科学技术学院 | 欢迎您！</title>
+    <script>(function(doc, win) {
+        var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange': 'resize',
+        recalc = function() {
+          var clientWidth = docEl.clientWidth;
+          if (!clientWidth) return;
+          docEl.style.fontSize = 12 * (clientWidth / 375) + 'px';
+        };
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+      })(document, window);
+      // 这里在375的设备上1rem = 20px，在其他屏幕宽的时候会自动根据这个比例来动态调整
+      </script>
+    <link rel="shortcut icon" href="http://web.fosu.edu.cn/wp-content/themes/fosu2019/images/favicon.ico" type="image/x-icon" />
+    <link href="<?php bloginfo('template_directory'); ?>/css/common-mob.css" type="text/css" rel="stylesheet" />
+    <!-- 主页 -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/index-mob.css">
+    <!-- <link rel="stylesheet" href="css/header.css"> -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/index-mob.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/owl.carousel.min.css">
+    <!-- 单独样式 -->
+    <!-- 空 不填-->
+    <script src="<?php bloginfo('template_directory'); ?>/js/jquery.min.js"></script>
+    <script>var _hmt = _hmt || []; (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?737248e8a6d0ec2e24bb8cfc179a8bc4";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+      })();</script>
+  </head>
+  <?php wp_head() ?>
---
+++ wp-theme/fosu/js/index-pc.js
@@ -0,0 +1,145 @@
+$(function(){
+
+  /*======= 功能函数定义 =======*/
+
+  /**
+   * [tab切换]
+   * @param  {[Object]} options [对象包含以下]
+   *  $tabX: tab的X元素集合
+      $tabY: tab的Y元素集合
+      event: 触发切换的事件类型，默认'mouseover',
+      xAtName: 当前X激活的类名，默认'active',
+      yAtName: 当前Y激活的类名，默认'active-box',
+      autoPlay: 自动轮播切换栏目，默认false
+      timeout: 自动轮播切换间隔，默认5000ms
+      hasMore: 是否支持MORE切换，默认false
+      moreSelec: MORE的选择字符串，默认'.column-more'
+      moreList: MORE的链接集合，默认空
+   */
+  function tab(options){
+    var opts = $.extend({}, tab.DEFAULTS, options);
+    if(opts.hasMore){ // 是否支持MORE的HREF值切换
+      var oMore = $(opts.moreSelec);
+    }
+    opts.$tabX.each(function(i){
+      $(this).on(opts.event, function(){
+        setActive(i);
+      })
+    });
+    
+    if(opts.autoPlay){  // 自动轮播时
+      var len = opts.$tabX.length;
+      var time;
+      if(len>1)
+      {
+        var _i = 0;
+        function autoloop(){
+          _i = ++_i<len ? _i : 0;
+          setActive(_i);
+        }
+        time = setInterval(autoloop, opts.timeout);
+        opts.$tabY.each(function(i){
+          $(this).on('mouseover', function(){
+            clearInterval(time);
+          }).on('mouseout', function(){
+            time = setInterval(autoloop, opts.timeout);
+          })
+        });
+      }
+    }
+
+    function setActive(i){
+      opts.$tabX.removeClass(opts.xAtName).eq(i).addClass(opts.xAtName);
+      opts.$tabY.removeClass(opts.yAtName).eq(i).addClass(opts.yAtName);
+      
+      oMore && oMore.attr('href', opts.moreList[i]);
+      
+    }
+  }
+  tab.DEFAULTS = {
+    $tabX: '',
+    $tabY: '',
+    event: 'mouseover',
+    xAtName: 'active',
+    yAtName: 'active-box',
+    autoPlay: false,
+    timeout: 5000,
+    hasMore:false,
+    moreSelec:'.column-more',
+    moreList:[]
+  };
+
+  /*======= 基本使用 =======*/
+
+  /*
+    首页幻灯片
+   */
+  $bigSlider = $(".big-slider");
+  $bigSlider.owlCarousel({
+    items:1,
+    nav: true,
+    navText:['',''],
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5640,
+    autoplaySpeed:2000,
+    autoplayHoverPause: true,
+    smartSpeed:1000,
+    lazyLoad: true
+  });
+
+  $bigSlider.on('changed.owl.carousel', function(e) {
+    // 幻灯片切换后的..
+    $cur_item = e.page.index;
+    $('.slider-box .banner li').removeClass('active').eq($cur_item).addClass('active');
+  });
+
+  /*
+    新闻头条幻灯片
+   */
+  $(".min-slider").owlCarousel({
+    items:1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3800,
+    autoplayHoverPause: true,
+    lazyLoad: true
+  });
+
+  /*
+    栏目Tab切换
+   */
+  tab({
+    $tabX: $('.column-headline-box .column-top>h3'),
+    $tabY: $('.column-headline-box .column-main>div'),
+    autoPlay: false,
+    hasMore: false
+  });
+
+  tab({
+    $tabX: $('.column-new-box .column-top>h3'),
+    $tabY: $('.column-new-box .column-main>ul'),
+    autoPlay: true,
+    timeout: 6800,
+    hasMore: true,
+    moreSelec:'.column-new-box .column-more',
+    moreList:[window.base_url+'/school-news', window.base_url+'/xueyuan-news', window.base_url+'/academic-forecast']
+  });
+
+  // tab({
+  //   $tabX: $('.column-msg-box .column-top>h3'),
+  //   $tabY: $('.column-msg-box .column-main>ul'),
+  //   hasMore: true,
+  //   moreSelec:'.column-msg-box .column-more',
+  //   moreList:[window.base_url+'/news-publication', 'http://web.fosu.edu.cn/gzc', window.base_url+'/academic-forecast']
+  // });
+
+  /*
+    在F12控制台打印招人信息
+   */
+  try {
+      window.console && window.console.log &&
+       (console.log("一张网页，要经历怎样的过程，才能抵达用户面前？\n一位新人，要经历怎样的成长，才能站在技术之巅？\n探寻这里的秘密；\n体验这里的挑战；\n成为这里的主人；\n加入澜达网络，加入我们，你，可以影响世界。\n"),console.log("请将简历发送至 %c lambdass@163.com（ 邮件标题请以“姓名-应聘XX职位-来自浏览器console”命名）", "color:red"), console.log("公司介绍：http://www.lambdass.com/"))
+  } catch(e) {}
+
+})
